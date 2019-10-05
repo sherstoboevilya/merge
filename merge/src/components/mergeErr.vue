29ba435e0e5d74a7e2a61ed04f68eb598510a137
ilya <sherstoboevilya@gmail.com>
2019-10-05 18:42:42 +0600

    create error

---
+++ merge/src/components/mergeErr.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>
+      Error
+    </h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "mergeErr"
+    }
+</script>
+
+<style scoped>
+
+</style>
