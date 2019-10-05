fa2e68a92b12b17d699974748d03165671cdff66
ilya <sherstoboevilya@gmail.com>
2019-10-05 17:31:55 +0600

    create second branch

---
+++ merge/src/components/newCompSec.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <h1>
+    Second component
+  </h1>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "newCompSec"
+    }
+</script>
+
+<style scoped>
+
+</style>
